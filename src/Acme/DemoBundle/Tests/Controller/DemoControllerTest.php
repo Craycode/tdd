1d4768eae957cb1d37ae799f44e60b6c4ea1bfa8
Piotr Suwik <piotr.suwik@mademedia.co.uk>
2014-06-08 14:05:49 +0100

    build improvements

--- src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
+++ src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
@@ -11,7 +11,7 @@ class DemoControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/demo/hello/Fabien');
-
+        $this->fail('I failed!');
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Hello Fabien")')->count());
     }
 
